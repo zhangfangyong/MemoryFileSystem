8262976c075cd50b05350ba16df5ad5b027f966d
zhangfangyong <15209805296@163.com>
2019-05-15 22:53:57 +0800

    Secend commit

---
+++ cmake-build-debug/CMakeFiles/MemoryFileSystem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MemoryFileSystem.dir/main.c.obj"
+  "MemoryFileSystem.pdb"
+  "MemoryFileSystem.exe"
+  "MemoryFileSystem.exe.manifest"
+  "libMemoryFileSystem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MemoryFileSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
